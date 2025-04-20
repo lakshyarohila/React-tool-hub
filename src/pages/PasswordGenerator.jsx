4c52741779348a50dcd24a827f89c061c27c17f9
lakshyarohila <lakshyarohila99@gmail.com>
2025-04-20 20:40:16 +0530

    bg

--- src/pages/PasswordGenerator.jsx
+++ src/pages/PasswordGenerator.jsx
@@ -32,7 +32,7 @@ const PasswordGenerator = () => {
   return (
     <div className="min-h-screen  flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl text-white font-bold mb-6">Generate Password</h1>
-      <div className="bg-white p-6 rounded-2xl shadow-2xl w-full max-w-md space-y-4">
+      <div className="bg-slate-700 p-6 rounded-2xl shadow-2xl w-full max-w-md space-y-4">
         <div>
           <label className="block font-medium">Length:{length}</label>
           <input
